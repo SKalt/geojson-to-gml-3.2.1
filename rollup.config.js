57e7a83ecb5eb9b3525f05926c07943bd0c05f38
SKalt <kalt.steven@gmail.com>
2017-06-12 12:54:56 -0400

    rollup init

---
+++ rollup.config.js
@@ -0,0 +1,33 @@
+import babel from 'rollup-plugin-babel';
+import eslint from 'rollup-plugin-eslint';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import replace from 'rollup-plugin-replace';
+import uglify from 'rollup-plugin-uglify';
+
+export default {
+  entry: 'geomToGml-3.2.1-es6.js',
+  dest: 'geomToGml-3.2.1.min.js',
+  format: 'iife',
+  sourceMap: 'inline',
+  plugins: [
+    resolve({
+      jsnext: true,
+      main: true,
+      browser: true,
+    }),
+    commonjs(),
+    eslint({
+      exclude: [
+        'src/styles/**',
+      ]
+    }),
+    babel({
+      exclude: 'node_modules/**',
+    }),
+    replace({
+      ENV: JSON.stringify(process.env.NODE_ENV || 'development'),
+    }),
+    (process.env.NODE_ENV === 'production' && uglify()),
+  ],
+};
